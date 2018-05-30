c784f984e10b1ed3bfb0fd13193b3986b43cfd0b
黄运飞 <huangyf1024@13322.com>
2018-05-30 17:39:28 +0800

    优化Button组件

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,35 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
-export default class Button extends React.Component{
-  static propTypes = {
-    disabled: PropTypes.bool,
-    type: PropTypes.string,
-    size: PropTypes.string,
-    plain: PropTypes.bool,
-    nativeType: PropTypes.string
-  }
-  static defaultProps = {
-    disabled: false,
-    type: 'default',
-    size: 'normal',
-    nativeType: 'button'
-  }
-  render () {
-    const { type, size, plain, className, disabled, nativeType, children, ...others } = this.props
-    const cls = classnames(['vx-btn', 'vx-btn-' + type, 'vx-btn-size-' + size, {
-      'is-plain': plain,
-      'is-disabled': disabled,
-    }, className])
-    
-    return (
-      <button 
-        {...others}
-        type={nativeType}
-        disabled={disabled}
-        className={cls}>
-          {children}
-      </button>
-    )
-  }
+const Button = (props) => {
+  const { type, size, plain, className, disabled, nativeType, children, ...others } = props
+  const cls = classnames(['vx-btn', 'vx-btn-' + type, 'vx-btn-size-' + size, {
+    'is-plain': plain,
+    'is-disabled': disabled,
+  }, className])
+  
+  return (
+    <button 
+      {...others}
+      type={nativeType}
+      disabled={disabled}
+      className={cls}>
+        {children}
+    </button>
+  )
 }
+
+Button.propTypes = {
+  disabled: PropTypes.bool,
+  type: PropTypes.string,
+  size: PropTypes.string,
+  plain: PropTypes.bool,
+  nativeType: PropTypes.string
+}
+Button.defaultProps = {
+  disabled: false,
+  type: 'default',
+  size: 'normal',
+  nativeType: 'button'
+}
+  
+export default Button
