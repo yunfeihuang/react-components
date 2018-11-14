69daaa4c860cfe477742d63fa9fd79aca59588a0
yunfei <yunfei.huang@qq.com>
2018-11-14 13:21:44 +0800

    优化成函数式组件

--- src/components/actionsheet/Actionsheet.js
+++ src/components/actionsheet/Actionsheet.js
@@ -3,66 +3,61 @@ import Popup from '../popup';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-class Actionsheet extends React.Component {
-  static propTypes = {
-    type: PropTypes.string,
-    open: PropTypes.bool,
-    cancel: PropTypes.bool,
-    cancelText: PropTypes.string,
-    title: PropTypes.string,
-    fastClose: PropTypes.bool,
-    onClose: PropTypes.func,
-    onClick: PropTypes.func
+const Actionsheet = props => {
+  let {children, className, title, cancelText, cancel, type, onClick, onClose, ...others} = props
+  
+  let handleClose = () => {
+    onClose && onClose()
   }
-  static defaultProps = {
-    type: 'default',
-    open: false,
-    cancel: false,
-    cancelText: '取消',
-    title: '',
-    fastClose: true
+  let handleClick = value => {
+    onClick && onClick(value)
+    handleClose()
   }
-  constructor (props) {
-    super(props)
-    this.handleClose = this.handleClose.bind(this)
-  }
-  render () {
-    let {children, className, title, cancelText, cancel, type, onClick, ...others} = this.props
-    let direction = type === 'menu' ? 'center' : 'bottom'
-    let cloneChildren = React.Children.map(children, item => {
-      if (item) {
-        return React.cloneElement(item, {
-          onClick: this.handleClick.bind(this, item.props.value)
-        })
-      }
-      return item
-    })
-    let cls = classnames(["vx-actionsheet", {'vx-actionsheet--menu': type === 'menu'}, {'is-not-title': !title}, className])
-    return (
-      <Popup className={cls} {...others} direction={direction}>
-        <div className="vx-actionsheet--inner" >
-          {title && <div className="vx-actionsheet--title">
-            {title}
-          </div>
-          }
-          <div className="vx-actionsheet--items">
-            {cloneChildren}
-          </div>
-          {cancel && <div className="vx-actionsheet--cancel" onClick={this.handleClose}>
-            {cancelText}
-          </div>
-          }
+  let direction = type === 'menu' ? 'center' : 'bottom'
+  let cloneChildren = React.Children.map(children, item => {
+    if (item) {
+      return React.cloneElement(item, {
+        onClick: handleClick.bind(this, item.props.value)
+      })
+    }
+    return item
+  })
+  let cls = classnames(["vx-actionsheet", {'vx-actionsheet--menu': type === 'menu'}, {'is-not-title': !title}, className])
+  return (
+    <Popup className={cls} {...others} direction={direction}>
+      <div className="vx-actionsheet--inner" >
+        {title && <div className="vx-actionsheet--title">
+          {title}
         </div>
-      </Popup>
-    );
-  }
-  handleClick (value) {
-    this.props.onClick && this.props.onClick(value)
-    this.handleClose()
-  }
-  handleClose () {
-    this.props.onClose && this.props.onClose()
-  }
+        }
+        <div className="vx-actionsheet--items">
+          {cloneChildren}
+        </div>
+        {cancel && <div className="vx-actionsheet--cancel" onClick={handleClose}>
+          {cancelText}
+        </div>
+        }
+      </div>
+    </Popup>
+  );
+}
+Actionsheet.propTypes = {
+  type: PropTypes.string,
+  open: PropTypes.bool,
+  cancel: PropTypes.bool,
+  cancelText: PropTypes.string,
+  title: PropTypes.string,
+  fastClose: PropTypes.bool,
+  onClose: PropTypes.func,
+  onClick: PropTypes.func
+}
+Actionsheet.defaultProps = {
+  type: 'default',
+  open: false,
+  cancel: false,
+  cancelText: '取消',
+  title: '',
+  fastClose: true
 }
 
 export default Actionsheet;
--- src/components/button-tab/ButtonTab.js
+++ src/components/button-tab/ButtonTab.js
@@ -2,35 +2,33 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-class ButtonTab extends React.Component {
-  static propTypes = {
-    active: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    size: PropTypes.string,
-    onChange: PropTypes.func
+const ButtonTab = props => {
+  let {children, className, active, size, onChange, ...others} = props
+  let handleClick = value => {
+    value !== active && onChange && onChange(value)
   }
-  static defaultProps = {
-    size: 'default'
-  }
-  render () {
-    let {children, className, active, size, ...others} = this.props
-    let cloneChildren = React.Children.map(children, item => {
-      if (item) {
-        return React.cloneElement(item, {
-          active,
-          onClick: this.handleClick.bind(this, item.props.name)
-        })
-      }
-      return item
-    })
-    return (
-      <div className={classnames(['vx-flexbox', 'vx-button-tab', `vx-button-tab--size-${size}`, className])} {...others}>
-        {cloneChildren}
-      </div>
-    );
-  }
-  handleClick (value) {
-    this.props.onChange && this.props.onChange(value)
-  }
+  let cloneChildren = React.Children.map(children, item => {
+    if (item) {
+      return React.cloneElement(item, {
+        active,
+        onClick: handleClick.bind(this, item.props.name)
+      })
+    }
+    return item
+  })
+  return (
+    <div className={classnames(['vx-flexbox', 'vx-button-tab', `vx-button-tab--size-${size}`, className])} {...others}>
+      {cloneChildren}
+    </div>
+  );
+}
+ButtonTab.propTypes = {
+  active: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  size: PropTypes.string,
+  onChange: PropTypes.func
+}
+ButtonTab.defaultProps = {
+  size: 'default'
 }
 
 export default ButtonTab;
