6a9585405b45c2d76abbd087b2ece6641318ae8c
yunfei <yunfei.huang@qq.com>
2018-11-16 23:11:34 +0800

    优化Toast组件

--- src/components/toast/index.js
+++ src/components/toast/index.js
@@ -31,15 +31,26 @@ class Toast extends React.Component {
   }
   render () {
     let {className, align, type, children, style, open} = this.props
-    const transitionStyles = {
-      entering: { opacity: 0, display: 'block'},
-      entered: { opacity: 0, display: 'block'}
+    const transitionClass = {
+      entering: 'confirm-fade-enter confirm-fade-enter-active',
+      entered: 'confirm-fade-enter-active',
+      exiting: 'confirm-fade-enter-active confirm-fade-leave-active',
+      exited: 'confirm-fade-leave'
     }
     return (
       <Transition in={open} timeout={120} onEntering={this.handleEntering}>
         {state => {
-          return (<div id={this.rootId} className={classnames(['vx-toast', 'vx-toast--' + align, className])}
-            style={{...style,...transitionStyles[state]}}>
+          let display = ''
+          if (state === 'entering') {
+            display = 'block'
+          }
+          if (state === 'exited') {
+            display = 'none'
+          }
+          return (
+          <div id={this.rootId}
+            className={classnames(['vx-toast', 'vx-toast--' + align, className, transitionClass[state]])}
+            style={{...style,display: display}}>
             <div className={classnames(['vx-toast--inner'])}>
               <div className="vx-toast--content">
                 {this.renderIcon()}
@@ -76,10 +87,10 @@ class Toast extends React.Component {
     requestAnimationFrame(() => {
       node.style.width = width + 'px'
       node.style.height = height + 'px'
-      node.style.opacity = 1
     })
+    this.$$timer && clearTimeout(this.$$timer)
     if (this.props.duration) {
-      setTimeout(() => {
+      this.$$timer = setTimeout(() => {
         this.props.onClose && this.props.onClose()
       }, this.props.duration)
     }
