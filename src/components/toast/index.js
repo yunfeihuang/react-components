29e5b1023de88a6fb50d314dd789450203794e63
yunfei <yunfei.huang@qq.com>
2018-11-16 18:07:09 +0800

    优化Toast组件

--- src/components/toast/index.js
+++ src/components/toast/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import Spinner from '../spinner'
 import classnames from 'classnames'
+import Transition from 'react-transition-group/Transition'
 
 class Toast extends React.Component {
   static propTypes = {
@@ -17,25 +17,39 @@ class Toast extends React.Component {
     duration: 2000,
     align: 'top',
     type: 'default',
-    open: false,
+    open: true,
     destroy: false,
     spinnerProps: {
       color:"#999",
       primaryColor:"#fff"
     }
   }
+  constructor (props) {
+    super(props)
+    this.handleEntering = this.handleEntering.bind(this)
+    this.rootId = Math.random().toString(36).substr(2)
+  }
   render () {
-    let {className, align, type, children, style} = this.props
+    let {className, align, type, children, style, open} = this.props
+    const transitionStyles = {
+      entering: { opacity: 0, display: 'block'},
+      entered: { opacity: 0, display: 'block'}
+    }
     return (
-      <div ref="$el" className={classnames(['vx-toast', 'vx-toast--' + align, className])} style={style}>
-        <div className={classnames(['vx-toast--inner'])}>
-          <div className="vx-toast--content">
-            {this.renderIcon()}
-            {type !== 'default' && <br/>}
-            {children}
-          </div>
-        </div>
-      </div>
+      <Transition in={open} timeout={120} onEntering={this.handleEntering}>
+        {state => {
+          return (<div id={this.rootId} className={classnames(['vx-toast', 'vx-toast--' + align, className])}
+            style={{...style,...transitionStyles[state]}}>
+            <div className={classnames(['vx-toast--inner'])}>
+              <div className="vx-toast--content">
+                {this.renderIcon()}
+                {type !== 'default' && <br/>}
+                {children}
+              </div>
+            </div>
+          </div>)
+        }}
+      </Transition>
     )
   }
   renderIcon () {
@@ -48,58 +62,26 @@ class Toast extends React.Component {
     return null
   }
   componentDidMount () {
-    this.openChange(this.props.open)
-  }
-  componentDidUpdate (prevProps) {
-    if (prevProps.open !== this.props.open) {
-      if (this.props.open) {
-        this.openChange(this.props.open)
-      } else {
-        if (!this.props.duration) {
-          this.refs.$el.style.display = 'none'
-        }
+    if (this.props.open) {
+      let node = document.getElementById(this.rootId)
+      if (node){
+        node.style.opacity = 0;
+        this.handleEntering(node)
       }
     }
   }
-  componentWillUnmount () {
-    this.$$timer && clearTimeout(this.$$timer)
-  }
-  css (text) {
-    if (this.refs.$el && this.refs.$el.style) {
-      this.refs.$el.style.cssText = text
-    }
-  }
-  hide () {
-    this.$$timer && clearTimeout(this.$$timer)
-    this.css('display:block;opacity:0;')
+  handleEntering (node) {
+    let width = node.children[0].offsetWidth + 4
+    let height = node.children[0].offsetHeight + 4
     requestAnimationFrame(() => {
-      this.css('display:none;')
-      this.props.onClose && this.props.onClose()
-      this.props.destroy && ReactDOM.unmountComponentAtNode(this.refs.$el)
+      node.style.width = width + 'px'
+      node.style.height = height + 'px'
+      node.style.opacity = 1
     })
-  }
-  openChange (value) {
-    if (value) {
-      this.$$timer && clearTimeout(this.$$timer)
-      requestAnimationFrame(() => {
-        this.css('display:block;opacity:0;')
-        requestAnimationFrame(() => {
-          if (this.refs.$el) {
-            let width = this.refs.$el.children[0].offsetWidth + 4
-            let height = this.refs.$el.children[0].offsetHeight + 4
-            requestAnimationFrame(() => {
-              this.css(`display:block;width:${width + 10}px;height:${height + 10}px;`)
-            })
-          }
-        })
-      })
-      if (this.props.duration) {
-        this.$$timer = setTimeout(() => {
-          this.hide()
-        }, this.props.duration)
-      }
-    } else {
-      this.hide()
+    if (this.props.duration) {
+      setTimeout(() => {
+        this.props.onClose && this.props.onClose()
+      }, this.props.duration)
     }
   }
 }
